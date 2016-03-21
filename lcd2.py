99db67634b7def5424a8dfa1b45fe838cffaadf9
carty64 <carty64@users.noreply.github.com>
2016-03-20 20:29:31 -0700

    Update lcd2.py

--- lcd2.py
+++ lcd2.py
@@ -14,8 +14,8 @@ lcd.begin(16, 1)
 sensor = W1ThermSensor()
 
 #temp_k = sensor.get_temperature(W1ThermSensor.KELVIN)
-   #temp_f = sensor.get_temperature(W1ThermSensor.DEGREES_F)
-   #temp_c = sensor.get_temperature(W1ThermSensor.DEGREES_C)
+#temp_f = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+#temp_c = sensor.get_temperature(W1ThermSensor.DEGREES_C)
 
 lcd.clear()
 
